64d4a2f3ced542a17f8d39ed655b03b3eac15441
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-09 21:47:58 +1000

    pycodestyle error

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,3 @@ for letter in range(ord("a"), ord("z") + 1):
     current_letter = chr(letter)
     if current_letter != 'q' and current_letter != 'e':
         print("{}".format(current_letter), end="")
-        
