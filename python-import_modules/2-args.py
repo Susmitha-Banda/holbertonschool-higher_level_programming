1d31db1565d8bbd3fe85b31eb27472f636bfbf13
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-13 19:49:59 +1000

    prints the number of and the list of its arguments.

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    arg = len(sys.argv) - 1
+    if arg == 0:
+        print("0 arguments.")
+    elif arg == 1:
+        print("{} argument:".format(arg))
+    else:
+        print("{} arguments:".format(arg))
+
+    for i in range(arg):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
