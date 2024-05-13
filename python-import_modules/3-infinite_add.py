513dd01204e29275383f03f61ce9100556ebc48b
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-13 19:56:43 +1000

    pycodestyle error

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 # output = addition of all args
-#we can cast args using int()
+# we can cast args using int()
 # no execution when imported
 import sys
-
 def main():
     # Initialize the sum
-    total = 0
-    
+    total = 0  
     # Loop through command-line arguments (excluding the script name)
     for arg in sys.argv[1:]:
         # Convert each argument to an integer and add it to the total
         total += int(arg)
-
     # Print the total sum
     print(total)
-
 if __name__ == "__main__":
     main()
