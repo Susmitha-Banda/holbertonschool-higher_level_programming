d9e323f0df90fe2c428b8034c5d0749849334442
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-18 11:54:42 +1000

    output error

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -18,18 +18,18 @@ class Square:
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        """Get the current size of the square."""
+        return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(size, value):
-            return TypeError("size must be an integer")
+        """Set the current size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
         elif value < 0:
-            return ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-
         """Return the current area of the square."""
-        return (self.__size * self.__size)
+        return self.__size * self.__size
