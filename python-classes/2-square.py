25edbdba92b45b20c30d1d6bfb05d4661eaf1940
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-18 11:35:41 +1000

    Output error - spelling mistake

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
             size (int): The size of the new square.
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an intiger")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
