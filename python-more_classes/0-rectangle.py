299390f7dc5015f913d49c4b590a53d9bb33f97c
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-21 19:58:13 +1000

    defining a rectangle for task 0 &1

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""creates an empty class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """create an empty rectangle object."""
+    pass
---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""creates an empty class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """create an empty rectangle object."""
+
+    def __init__(self, width=0, height=0):
+        """initialising a new rectangle
+        Args:
+        width (int): width of the rectangle.
+        height (int): height of the rectangle.
+        """
+        self.width
+        self.height
+
+    @property
+    def width(self):
+        """get the width of the rectangle"""
+        return self.width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get the height of the rectangle"""
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
