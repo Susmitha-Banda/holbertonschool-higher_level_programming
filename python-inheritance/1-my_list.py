d2658860089a9a69499e2977b9261814fd3df430
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-22 14:34:07 +1000

    indentation error

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,10 +6,10 @@
 class MyList(list):
     ''' Represents a MyList
     '''
-
-
-def print_sorted(self):
-    '''
+    
+    
+    def print_sorted(self):
+        '''
         prints the list, but sorted
-    '''
-    print(sorted(self))
+        '''
+        print(sorted(self))
