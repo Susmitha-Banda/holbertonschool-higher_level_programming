34ae9287d5f3270e85a883369e0235ad17d626ee
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-22 20:57:59 +1000

    print() should print, and str() should return

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+This moudle has a class Square that inherits
+from Rectangle with some methods
+'''
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Defines a square class that inherits from Rectangle'''
+
+    def __init__(self, size):
+        '''Initializes a square with size
+        validating the size value first
+        to catch any invalid values early
+        '''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
