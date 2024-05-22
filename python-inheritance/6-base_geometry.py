f5213c9364fe637dc655de1acda6db687e4f44b4
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-22 14:59:24 +1000

    Public instance method: def area(self):

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is an empty BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    ''' Class: BaseGeometry
+    '''
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
