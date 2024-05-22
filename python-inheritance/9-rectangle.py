3c53839375445da3a8c8055646c3fe0dac74b450
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-22 20:42:32 +1000

    defining area of a rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" implements a Geometry class"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """implementing rectangle class"""
+    def __init__(self, width, height):
+        """Instantiation with width and height"""
+        self.__width = width
+        self.integer_validator("width", width)
+        self.__height = height
+        self.integer_validator("height", height)
+
+    def area(self):
+        """defining area of a rectangle"""
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return ("[Rectangle]" + str(self.__width) + "/" + str(self.__height))
