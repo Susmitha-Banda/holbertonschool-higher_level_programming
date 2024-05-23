05372c3c97f9e23039259ffedbaacfa0ad8180c8
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-23 14:14:19 +1000

    Abstract Animal Class and its Subclasses

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""this module is about creating an abstract class named Animal using the ABC
+package."""
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """this is an abstract class Animal"""
+    @abstractmethod
+    def sound(self):
+        """abstract method for making a sound"""
+        pass
+
+
+class dog(Animal):
+    """this is a dog subclass of Animal"""
+    def sound(self):
+        """sound method of dog class"""
+        return ("Bark")
+
+
+class cat(Animal):
+    """this is a cat subclass of Animal"""
+    def sound(self):
+        """sound method of cat class"""
+        return ("Meow")
