ced9299a26a7121dd0fb2a7c25a4b9134854447b
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-24 18:38:22 +1000

    function that returns the JSON representation of an object (string):

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""function that returns the JSON representation of an object (string):"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """module to_json_strin
+     returns JSON representation"""
+    if isinstance(my_obj, set):
+        '''the json.dumps() function in Python does not support
+        serializing set objects directly to JSON.'''
+        return json.dumps(list(my_obj))
+    return json.dumps(my_obj)
