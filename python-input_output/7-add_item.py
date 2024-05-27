9012cc5d698f76e752462a130dddc730c39f7428
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-27 20:41:37 +1000

    missing a module-level docstring

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This script adds all command line arguments to a Python list,
+and then saves them to a file named add_item.json.
+
+- If the file doesn’t exist, it should be created.
+- The list is saved as a JSON representation in the file add_item.json.
+"""
+
 import sys
 import os
 
