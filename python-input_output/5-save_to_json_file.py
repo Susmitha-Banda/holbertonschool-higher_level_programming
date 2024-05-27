7ea221cfed26549a7978fc69ade3de31a9a7a0b6
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-27 19:49:33 +1000

    The directory no_perm has been set with restrictive permissions (400), meaning no write permissions are available.

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -10,5 +10,15 @@ def save_to_json_file(my_obj, filename):
     ''' module save_to_json_file
     accepts Python object and sends JSON representation to file
     '''
-    with open(filename, "w") as f:
-        f.write(json.dumps(my_obj))
+
+    try:
+        # Convert set to list to make it JSON serializable
+        if isinstance(my_obj, set):
+            my_obj = list(my_obj)
+
+        with open(filename, "w") as f:
+            f.write(json.dumps(my_obj))
+    except TypeError as e:
+        print(f"[TypeError] {e}")
+    except PermissionError as e:
+        print(f"[PermissionError] {e}")
