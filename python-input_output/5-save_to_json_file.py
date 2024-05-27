448121379804f9a43dc7cfd9d3a1ad016dda2944
Susmitha Banda <susmithamurthy9@gmail.com>
2024-05-27 19:54:37 +1000

    TypeError("Object of type set is not JSON serializable")

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -2,7 +2,6 @@
 """function that writes an Object to a text file,
 using a JSON representation"""
 
-
 import json
 
 
@@ -10,11 +9,10 @@ def save_to_json_file(my_obj, filename):
     ''' module save_to_json_file
     accepts Python object and sends JSON representation to file
     '''
-
     try:
         # Convert set to list to make it JSON serializable
         if isinstance(my_obj, set):
-            my_obj = list(my_obj)
+            raise TypeError("Object of type set is not JSON serializable")
 
         with open(filename, "w") as f:
             f.write(json.dumps(my_obj))
