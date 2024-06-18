91534b54f16752480428368c5cb15f0e038b82ac
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-06-18 13:38:00 +1000

    script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,9 @@
 -- script that creates a table called first_table in the current database in your MySQL server.
 -- If the table first_table already exists, your script should not fail
 
+USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
+    `id` INT PRIMARY KEY,
+    `name` VARCHAR(256) NOT NULL
+ 
 );
