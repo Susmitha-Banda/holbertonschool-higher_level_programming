41055056ebe57b280b7c7e11418afef676ab287c
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-06-18 13:48:56 +1000

    error handling in task 5

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -2,8 +2,10 @@
 -- If the table first_table already exists, your script should not fail
 
 USE hbtn_0c_0;
-CREATE TABLE IF NOT EXISTS first_table (
-    `id` INT PRIMARY KEY,
-    `name` VARCHAR(256) NOT NULL
- 
+-- creates a table called first_table in the current database in MySQL server.
+
+CREATE table if not exists first_table(
+    id INT NOT NULL,
+    name VARCHAR (256) NOT NULL,
+    PRIMARY KEY (id)
 );
--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,4 @@
--- script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
--- The database name will be passed as an argument of the mysql command
--- You are not allowed to use the DESCRIBE or EXPLAIN statements
+-- prints the following description of the table first_table 
+-- from the database hbtn_0c_0 in your MySQL server.
 
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+SHOW CREATE TABLE first_table;
