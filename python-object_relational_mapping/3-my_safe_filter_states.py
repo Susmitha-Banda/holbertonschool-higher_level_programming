9f2913f80184c4c8a868cd55c05f59d04552574a
Susmitha-Banda <susmithamurthy9@gmail.com>
2024-06-21 11:50:45 +1000

    Updated the code

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,20 +2,13 @@
 """Script that takes in an argument and displays all values in the states table
 of hbtn_0e_0_usa where name matches the argument, safe from SQL injection"""
 
-
 import MySQLdb
 import sys
-
 
 if __name__ == "__main__":
     # ensure the correct number of command line args are provided
     if len(sys.argv) != 5:
-        print("Usage: ./3-my_safe_filter_states.py"
-              "<mysql username>"
-              "<mysql password>"
-              "<database name>"
-              "<state name>"
-              )
+        print("Usage: ./3-my_safe_filter_states.py <mysql username> <mysql password> <database name> <state name>")
         sys.exit(1)
 
     # get the command-line arguments
@@ -32,22 +25,28 @@ if __name__ == "__main__":
             user=mysql_username,
             passwd=mysql_password,
             db=db_name
-            )
+        )
+
+        print("Connected to MySQL successfully")
+
         # create a cursor object to interact with the database
         cursor = db.cursor()
-        # create a sql query using a
-        # parameterised query to prevent SQL injection
-        query = (
-            "SELECT * FROM states WHERE BINARY name = '{0}' "
-            "ORDER BY id ASC".format(sys.argv[4])
-        )
-        # execute the query with the provided state nane
+
+        # create a sql query using a parameterised query to prevent SQL injection
+        query = "SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
+        
+        print("Executing query:", query % state_name)
+
+        # execute the query with the provided state name
         cursor.execute(query, (state_name,))
+
         # fetch all rows from the executed query
         rows = cursor.fetchall()
+
         # loop through the rows and print each one
         for row in rows:
             print("({}, '{}')".format(row[0], row[1]))
+
         # close the cursor and db connection
         cursor.close()
         db.close()
